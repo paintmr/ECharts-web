8a8d1324f52ba25a14584ca10f6a2e3ab06f7b38
chujingxl <chujingxl@gmail.com>
2021-05-06 22:17:16 +0800

    top

--- js/index.js
+++ js/index.js
@@ -518,3 +518,133 @@ myChart.setOption(option);
 window.addEventListener("resize", function () {
   myChart.resize();
 });
+// 全国热榜
+(function () {
+  // (1) 准备相关数据
+  var hotData = [
+    {
+      city: "北京", // 城市
+      sales: "25,179", // 销售额
+      flag: true, //  上升还是下降
+      brands: [
+        //  品牌种类数据
+        { name: "可爱多", num: "9,086", flag: true },
+        { name: "娃哈哈", num: "8,341", flag: true },
+        { name: "喜之郎", num: "7,407", flag: false },
+        { name: "八喜", num: "6,080", flag: false },
+        { name: "小洋人", num: "6,724", flag: false },
+        { name: "好多鱼", num: "2,170", flag: true }
+      ]
+    },
+    {
+      city: "河北",
+      sales: "23,252",
+      flag: false,
+      brands: [
+        { name: "可爱多", num: "3,457", flag: false },
+        { name: "娃哈哈", num: "2,124", flag: true },
+        { name: "喜之郎", num: "8,907", flag: false },
+        { name: "八喜", num: "6,080", flag: true },
+        { name: "小洋人", num: "1,724", flag: false },
+        { name: "好多鱼", num: "1,170", flag: false }
+      ]
+    },
+    {
+      city: "上海",
+      sales: "20,760",
+      flag: true,
+      brands: [
+        { name: "可爱多", num: "2,345", flag: true },
+        { name: "娃哈哈", num: "7,109", flag: true },
+        { name: "喜之郎", num: "3,701", flag: false },
+        { name: "八喜", num: "6,080", flag: false },
+        { name: "小洋人", num: "2,724", flag: false },
+        { name: "好多鱼", num: "2,998", flag: true }
+      ]
+    },
+    {
+      city: "江苏",
+      sales: "23,252",
+      flag: false,
+      brands: [
+        { name: "可爱多", num: "2,156", flag: false },
+        { name: "娃哈哈", num: "2,456", flag: true },
+        { name: "喜之郎", num: "9,737", flag: true },
+        { name: "八喜", num: "2,080", flag: true },
+        { name: "小洋人", num: "8,724", flag: true },
+        { name: "好多鱼", num: "1,770", flag: false }
+      ]
+    },
+    {
+      city: "山东",
+      sales: "20,760",
+      flag: true,
+      brands: [
+        { name: "可爱多", num: "9,567", flag: true },
+        { name: "娃哈哈", num: "2,345", flag: false },
+        { name: "喜之郎", num: "9,037", flag: false },
+        { name: "八喜", num: "1,080", flag: true },
+        { name: "小洋人", num: "4,724", flag: false },
+        { name: "好多鱼", num: "9,999", flag: true }
+      ]
+    }
+  ];
+  // (2) 根据数据渲染各省热销sup模块内容
+  // (2-1) 删掉原先自带的li
+  // (2-2) 遍历数据$.each()
+  // (2-3) 拼接字符串把数据渲染到li的span里面
+  var supHTML = '';
+  $.each(hotData, function (index, item) {
+    supHTML += `<li>
+    <span>${item.city}</span>
+    <span>${item.sales}<s class=${item.flag ? "icon-up" : "icon-down"}></s></span>
+  </li>`
+  })
+  // (2-4) 追加给sup盒子。必须通过jQuery的html方法把supHTML字符串转换为标签。
+  $('.sup').html(supHTML)
+  // (3) 鼠标进入sup的li，高亮li，显示对应城市的sub销售情况
+  // (3-1) 当前的li高亮，其它的li复原
+  $('.province .sup').on('mouseenter', 'li', function () {
+    index = $(this).index(); //把当前li的序号告诉index，以便鼠标离开开启定时器时计数从当前的li开始计数。
+    renderSupSubHTML($(this));
+  })
+  // (4) 一进入页面，就把sup的第1个li选中，把hotData[0].brands的数据渲染出来
+  var lis = $('.province .sup li');
+  lis.eq(0).mouseenter();
+  // (5) 开启定时器
+  var index = 0;
+  var timer = setInterval(function () {
+    index++;
+    if (index >= 5) index = 0;
+    // lis.eq(index).mouseenter();
+    renderSupSubHTML(lis.eq(index));
+  }, 1000);
+  // (6) 鼠标经过停止和开启定时器
+  $('.province .sup').hover(
+    function () {
+      clearInterval(timer);
+    }, function () {
+      clearInterval(timer);
+      timer = setInterval(function () {
+        index++;
+        if (index >= 5) index = 0;
+        // lis.eq(index).mouseenter();
+        renderSupSubHTML(lis.eq(index));
+      }, 1000);
+    }
+  )
+  // (7)把按顺序渲染sup和sub封装到函数里
+  function renderSupSubHTML(currentEle, i) {
+    currentEle.addClass('active').siblings().removeClass();
+    //  遍历当前索引号对应的hotData里面的brands数据。hotData[$(this).index()].brands
+    var subHTML = '';
+    $.each(hotData[currentEle.index()].brands, function (index, item) {
+      subHTML += `<li>
+      <span>${item.name}</span><s class=${item.flag ? "icon-up" : "icon-down"} ></s
+    > <span>${item.num}</span>
+    </li > `
+      // 把subHTML放到sub盒子里。必须通过jQuery的html方法把subHTML字符串转换为标签。
+      $('.sub').html(subHTML);
+    });
+  }
+})();
